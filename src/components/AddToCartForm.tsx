88d26fc29ecfc8fe56773866ee0bcd6c5619a853
Piterxus <piterxus@gmail.com>
2024-10-22 11:54:55 +0200

    feat: Convert Icon into a button

--- src/components/AddToCartForm.tsx
+++ src/components/AddToCartForm.tsx
@@ -2,12 +2,31 @@ import { isCartOpen, addCartItem } from '../cartStore';
 import type { CartItemDisplayInfo } from '../cartStore';
 import type { ReactNode } from 'react';  // Importa ReactNode en lugar de Children ya que que Children en React no es un tipo, sino un valor que hace referencia al API de utilidades para manipular los hijos de un componente.
 
+// type Props = {
+//     item: CartItemDisplayInfo;
+//     children: ReactNode;  // Usa ReactNode para definir los hijos
+// };
+// Prescindo por el momento de item
 type Props = {
-    item: CartItemDisplayInfo;
+ 
     children: ReactNode;  // Usa ReactNode para definir los hijos
 };
+// export default function AddToCartForm({ item, children }: Props) {
+//     // function addToCart(e: React.FormEvent<HTMLFormElement>) {  // Ajusta el tipo del evento
+//     //     e.preventDefault();
+//     //     isCartOpen.set(true);
+//     //     addCartItem(item);
+//     // }
+//     function addToCart(e: React.FormEvent<HTMLFormElement>) {
+//         e.preventDefault();
+//         alert('Añadir al carrito');
+//         console.log('Añadir al carrito');
+//     }
 
-export default function AddToCartForm({ item, children }: Props) {
+//     return <form onSubmit={addToCart}>{children}</form>;
+// }
+
+export default function AddToCartForm({ children }: Props) {
     // function addToCart(e: React.FormEvent<HTMLFormElement>) {  // Ajusta el tipo del evento
     //     e.preventDefault();
     //     isCartOpen.set(true);
@@ -15,6 +34,7 @@ export default function AddToCartForm({ item, children }: Props) {
     // }
     function addToCart(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
+        alert('Añadir al carrito');
         console.log('Añadir al carrito');
     }
 
