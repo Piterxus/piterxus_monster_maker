4e78e38e44eb98f93a1a33559f512c744612b449
Piterxus <piterxus@gmail.com>
2024-10-23 11:51:30 +0200

    feat: Implementing functionality add to cart

--- src/components/CartFlyout.tsx
+++ src/components/CartFlyout.tsx
@@ -12,7 +12,7 @@ export default function CartFlyout() {
                 <ul className={styles.list} role="list">
                     {Object.values($cartItems).map((cartItem) => (
                         <li className={styles.listItem}>
-                            <img className={styles.listItemImg} src={cartItem.imageSrc} alt={cartItem.name} />
+                            {/* <img className={styles.listItemImg} src={cartItem.imageSrc} alt={cartItem.name} /> */}
                             <div>
                                 <h3>{cartItem.name}</h3>
                                 <p>Quantity: {cartItem.quantity}</p>
