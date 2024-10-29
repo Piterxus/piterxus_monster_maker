319d3ef930a2168c407831735ddcffeb3c896bfa
Piterxus <piterxus@gmail.com>
2024-10-29 10:55:42 +0100

    feat: Testing Stripe API requests

--- src/lib/get-card-info.ts
+++ src/lib/get-card-info.ts
@@ -1,4 +1,5 @@
 import { query } from './strapi';
+const STRAPI_HOST = import.meta.env.PUBLIC_STRAPI_HOST;
 
 // export async function getCardInfo (id: string) {
 //     return query(`cards/${id}`);
@@ -10,9 +11,12 @@ import { query } from './strapi';
 // }
 
 export function getCardInfo() {
-    return query(`cards`)
+    return query(`cards?populate=img_product`)
         .then((res) => {
             console.log(res.data);
+            const image = `${STRAPI_HOST}/${res.data[0].img_product[0].url}`;
+            console.log(image);
+            // console.log(res.data[0].img_product[0].url);
             return res.data;
         });
 }
