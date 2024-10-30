862f937029cd7364da288a6fc80c0d5177998e35
Piterxus <piterxus@gmail.com>
2024-10-30 21:20:38 +0100

    test: Change astro components for React

--- src/lib/get-card-info.ts
+++ src/lib/get-card-info.ts
@@ -13,7 +13,7 @@ const STRAPI_HOST = import.meta.env.PUBLIC_STRAPI_HOST;
 export function getCardInfo() {
     return query(`cards?populate=img_product`)
         .then((res) => {
-            console.log(res.data);
+            // console.log(res.data);
             const image = `${STRAPI_HOST}${res.data[0].img_product[0].url}`;
             console.log(image);
             // console.log(res.data[0].img_product[0].url);
