bf85d7be3d011502b22679d384c660ed3e0d7966
Piterxus <piterxus@gmail.com>
2024-11-02 21:44:22 +0100

    feat: Add ReactRoutes component

---
+++ src/components/ReactRoutes.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Cart from './Cart';
+
+const ReactRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/cart" element={<Cart />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default ReactRoutes;
