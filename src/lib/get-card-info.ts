0e8cf4c73357b953ee21a4de1f7bc08388150139
Piterxus <piterxus@gmail.com>
2024-11-03 19:06:21 +0100

    chore: Delete unnecessary comments and statements

--- src/lib/get-card-info.ts
+++ src/lib/get-card-info.ts
@@ -6,10 +6,9 @@ const STRAPI_HOST = import.meta.env.PUBLIC_STRAPI_HOST;
 export function getCardInfo() {
     return query(`cards?populate=img_product`)
         .then((res) => {
-            // console.log(res.data);
+           
             const image = `${STRAPI_HOST}${res.data[0].img_product[0].url}`;
-            console.log(image);
-            // console.log(res.data[0].img_product[0].url);
+           
             return res.data;
         });
 }
