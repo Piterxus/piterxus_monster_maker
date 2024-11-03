4eec1580d9f39d495d87380f83360fb63945ee40
Piterxus <piterxus@gmail.com>
2024-11-03 18:56:25 +0100

    feat: Implement cart

--- src/cartStore.ts
+++ src/cartStore.ts
@@ -16,19 +16,7 @@ export const cartItems = map<Record<string, CartItem>>({});
 
 export function addCartItem({ id, name, imageSrc, price }: CartItemDisplayInfo) {
     const existingEntry = cartItems.get()[id];
-    // if (existingEntry) {
-    //     cartItems.setKey(id, {
-    //         ...existingEntry,
-    //         quantity: existingEntry.quantity + 1,
-    //     });
-    // } else {
-    //     cartItems.setKey(id, {
-    //         id,
-    //         name,
-    //         imageSrc,
-    //         quantity: 1,
-    //     });
-    // }
+
     if (!existingEntry) {
         cartItems.setKey(id, {
                     id,
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,7 +1,28 @@
+import styles from "../styles/Cart.module.css";
+import { useStore } from '@nanostores/react';
+import { cartItems } from '../cartStore';
+
 const Cart = () => {
+    const $cartItems = useStore(cartItems);
+    const cartItemsArray = Object.values($cartItems);
     return (
         <div>
-            <h1>Cart</h1>
+            <h2>Cart</h2>
+            {cartItemsArray.length ? (
+                <ul className={styles.cart_list}>
+                    {cartItemsArray.map((item) => (
+                        <li key={item.id} className={styles.cart_item}>
+                            <img src={item.imageSrc} alt="Product" />
+                            <div>
+                                <p>{item.name}</p>
+                                <p>{item.price} €</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>Your cart is empty!</p>
+            )}
         </div>
     );
 };
