9fa6ccb89c4e36628fd811e7fc614136945224bb
Piterxus <piterxus@gmail.com>
2024-11-09 21:04:59 +0100

    feat: Use aliases to import styles

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,4 +1,4 @@
-import styles from "../styles/Cart.module.css";
+import styles from "@styles/Cart.module.css";
 import { useStore } from '@nanostores/react';
 import { cartItems, removeItemFromCart } from '../cartStore';
 import Icon from "./Icon";
