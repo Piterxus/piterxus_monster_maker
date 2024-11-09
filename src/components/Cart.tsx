406071dd151733ed2fdcd1eb592c24ea0b510365
Piterxus <piterxus@gmail.com>
2024-11-09 20:11:39 +0100

    styles: Add a new style to cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,23 +1,28 @@
 import styles from "../styles/Cart.module.css";
 import { useStore } from '@nanostores/react';
 import { cartItems, removeItemFromCart } from '../cartStore';
+import Icon from "./Icon";
 
 const Cart = () => {
     const $cartItems = useStore(cartItems);
     const cartItemsArray = Object.values($cartItems);
+    const total = cartItemsArray.reduce((acc, item) => acc + item.price, 0);
 
     return (
-        <div>
-            <h2>Cart</h2>
+        <div className={styles.cart_container}>
+            <h2>Your cart</h2>
             {cartItemsArray.length ? (
                 <ul className={styles.cart_list}>
                     {cartItemsArray.map((item) => (
                         <li key={item.id} className={styles.cart_item}>
                             <img src={item.imageSrc} alt="Product" />
-                            <div>
-                                <p>{item.name}</p>
+                            <div className={styles.item_info} onClick={() => removeItemFromCart(item.id)}>
                                 <p>{item.price} €</p>
-                                <button onClick={() => removeItemFromCart(item.id)}>Remove</button>
+                                <Icon
+                                    Imgsrc="/imgs/skeleton_shopping_remove_cart.png"
+                                    alt="Cart"
+                                    tooltipText="Remove to cart"
+                                />
                             </div>
                         </li>
                     ))}
@@ -25,6 +30,7 @@ const Cart = () => {
             ) : (
                 <p>Your cart is empty!</p>
             )}
+            <p>Total: {total} €</p>
         </div>
     );
 };
