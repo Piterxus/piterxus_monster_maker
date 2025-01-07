3d2c5ffbf9c939c6b143fcc81160808d1a813294
Piterxus <piterxus@gmail.com>
2025-01-07 11:53:16 +0100

    feat: Implement purchase form

--- src/components/Buy.tsx
+++ src/components/Buy.tsx
@@ -1,21 +1,34 @@
 import { useStore } from '@nanostores/react';
 import { cartItems, addItemToCart } from '../cartStore';
+import styles from "../styles/Buy.module.css";
 
 
-const Buy = () =>{
+const Buy = () => {
     const $cartItems = useStore(cartItems);
 
     if (Object.keys($cartItems).length === 0) {
         // return <p>No items in cart</p>;
-    //   alert("No items in cart");
-    window.location.href = "/";
+        //   alert("No items in cart");
+        window.location.href = "/";
     }
-    // return (
-    //     <div>
-    //         <h2>Buy</h2>
-    //         <button>Buy</button>
-    //     </div>
-    // );
+    return (
+        <div className={styles.buyContainer}>
+            <form className='buyForm'>
+                <h2>Buy</h2>
+                <div className='{styles.inputContainer'>
+                    <label htmlFor='name'>Name:</label>
+                    <input type='text' id='name' name='name' required />
+                </div>
+                <div className='{styles.inputContainer'>
+                    <label htmlFor='email'>Email:</label>
+                    <input type='email' id='email' name='email' required />
+                </div>
+
+
+                <button>Buy</button>
+            </form>
+        </div>
+    );
 
 }
 
