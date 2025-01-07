166381ff468dc9e359a97aeec65af145d2382e98
Piterxus <piterxus@gmail.com>
2025-01-07 12:44:43 +0100

    feat: Implement purchase form

--- src/components/Buy.tsx
+++ src/components/Buy.tsx
@@ -15,30 +15,41 @@ const Buy = () => {
         <div className={styles.buyContainer}>
             <form className='buyForm'>
                 <h2>Buy</h2>
-                <div className={styles.inputContainer}>
-                    <label htmlFor='name'>Name:</label>
-                    <input type='text' id='name' name='name' required />
-                </div>
-                <div className={styles.inputContainer}>
-                    <label htmlFor='email'>Email:</label>
-                    <input type='email' id='email' name='email' required />
-                </div>
-                <div className={styles.inputContainer}>
-                    <label htmlFor='phone'>Phone:</label>
-                    <input type='tel' id='phone' name='phone' required />
-                </div>
-                <div className={styles.inputContainer}>
-                    <label htmlFor='address'>Address:</label>
-                    <input type='text' id='address' name='address' required />
-                </div>
-                <div className={styles.inputContainer}>
-                    <label htmlFor='city'>City:</label>
-                    <input type='text' id='city' name='city' required />
-                </div>
-                <div className={styles.inputContainer}>
-                    <label htmlFor='zip'>Zip:</label>
-                    <input type='text' id='zip' name='zip' required />
-                </div>
+            <div className={styles.buyInfo}>
+             <div className={styles.contactInfo}>
+                        <div className={styles.inputContainer}>
+                            <label htmlFor='name'>Name:</label>
+                            <input type='text' id='name' name='name' required />
+                        </div>
+                        <div className={styles.inputContainer}>
+                            <label htmlFor='email'>Email:</label>
+                            <input type='email' id='email' name='email' required />
+                        </div>
+                        <div className={styles.inputContainer}>
+                            <label htmlFor='phone'>Phone:</label>
+                            <input type='tel' id='phone' name='phone' required />
+                        </div>
+                        <div className={styles.inputContainer}>
+                            <label htmlFor='address'>Address:</label>
+                            <input type='text' id='address' name='address' required />
+                        </div>
+                        <div className={styles.inputContainer}>
+                            <label htmlFor='city'>City:</label>
+                            <input type='text' id='city' name='city' required />
+                        </div>
+                        <div className={styles.inputContainer}>
+                            <label htmlFor='zip'>Zip:</label>
+                            <input type='text' id='zip' name='zip' required />
+                        </div>
+             </div>
+             <div className={styles.paymentInfo}>
+                    <select name="" id="">
+                        <option value="1">Credit Card</option>
+                        <option value="2">Debit Card</option>
+                        <option value="3">Paypal</option>
+                    </select>
+             </div>
+            </div>
 
 
                 <button>Buy</button>
