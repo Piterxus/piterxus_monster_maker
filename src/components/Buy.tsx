55d0050fb863643ef503ae1d2054e45c92050fcc
Piterxus <piterxus@gmail.com>
2025-01-07 12:34:30 +0100

    feat: Implement purchase form

--- src/components/Buy.tsx
+++ src/components/Buy.tsx
@@ -15,13 +15,29 @@ const Buy = () => {
         <div className={styles.buyContainer}>
             <form className='buyForm'>
                 <h2>Buy</h2>
-                <div className='{styles.inputContainer'>
+                <div className={styles.inputContainer}>
                     <label htmlFor='name'>Name:</label>
                     <input type='text' id='name' name='name' required />
                 </div>
-                <div className='{styles.inputContainer'>
+                <div className={styles.inputContainer}>
                     <label htmlFor='email'>Email:</label>
                     <input type='email' id='email' name='email' required />
+                </div>
+                <div className={styles.inputContainer}>
+                    <label htmlFor='phone'>Phone:</label>
+                    <input type='tel' id='phone' name='phone' required />
+                </div>
+                <div className={styles.inputContainer}>
+                    <label htmlFor='address'>Address:</label>
+                    <input type='text' id='address' name='address' required />
+                </div>
+                <div className={styles.inputContainer}>
+                    <label htmlFor='city'>City:</label>
+                    <input type='text' id='city' name='city' required />
+                </div>
+                <div className={styles.inputContainer}>
+                    <label htmlFor='zip'>Zip:</label>
+                    <input type='text' id='zip' name='zip' required />
                 </div>
 
 
