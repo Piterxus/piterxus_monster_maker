a00454a5e77dcb3d2ac23d33171607d77be469d8
Piterxus <piterxus@gmail.com>
2025-03-21 13:12:01 +0100

    feat: Replace buy form for Stripe Element

--- src/components/Buy.tsx
+++ src/components/Buy.tsx
@@ -1,6 +1,11 @@
 import { useStore } from '@nanostores/react';
 import { cartItems, addItemToCart } from '../cartStore';
 import styles from "../styles/Buy.module.css";
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+const publishableKey = import.meta.env.PUBLIC_STRIPE_PUBLIC_KEY;
+const stripePromise = loadStripe(publishableKey);
 
 
 const Buy = () => {
@@ -13,54 +18,57 @@ const Buy = () => {
         window.location.href = "/";
     }
     return (
-        <div className={styles.buyContainer}>
-            <form className='buyForm'>
-                <h2>Buy</h2>
-            <div className={styles.buyInfo}>
-             <div className={styles.contactInfo}>
-                        <div className={styles.inputContainer}>
-                            <label htmlFor='name'>Name:</label>
-                            <input type='text' id='name' name='name' required />
-                        </div>
-                        <div className={styles.inputContainer}>
-                            <label htmlFor='email'>Email:</label>
-                            <input type='email' id='email' name='email' required />
-                        </div>
-                        <div className={styles.inputContainer}>
-                            <label htmlFor='phone'>Phone:</label>
-                            <input type='tel' id='phone' name='phone' required />
-                        </div>
-                        <div className={styles.inputContainer}>
-                            <label htmlFor='address'>Address:</label>
-                            <input type='text' id='address' name='address' required />
-                        </div>
-                        <div className={styles.inputContainer}>
-                            <label htmlFor='country'>Country:</label>
-                            <input type='text' id='country' name='country' required />
-                        </div>
-                        <div className={styles.inputContainer}>
-                            <label htmlFor='city'>City:</label>
-                            <input type='text' id='city' name='city' required />
-                        </div>
-                        <div className={styles.inputContainer}>
-                            <label htmlFor='zip'>Zip:</label>
-                            <input type='text' id='zip' name='zip' required />
-                        </div>
-             </div>
-             <div className={styles.paymentInfo}>
-                    <select name="" id="">
-                        <option value="1">Credit Card</option>
-                        <option value="2">Debit Card</option>
-                        <option value="3">Paypal</option>
-                    </select>
-                <p>TOTAL: {total} €</p>
-             </div>
-            </div>
+        <Elements stripe={stripePromise}>
+
+        </Elements>
+        // <div className={styles.buyContainer}>
+        //     <form className='buyForm'>
+        //         <h2>Buy</h2>
+        //         <div className={styles.buyInfo}>
+        //             <div className={styles.contactInfo}>
+        //                 <div className={styles.inputContainer}>
+        //                     <label htmlFor='name'>Name:</label>
+        //                     <input type='text' id='name' name='name' required />
+        //                 </div>
+        //                 <div className={styles.inputContainer}>
+        //                     <label htmlFor='email'>Email:</label>
+        //                     <input type='email' id='email' name='email' required />
+        //                 </div>
+        //                 <div className={styles.inputContainer}>
+        //                     <label htmlFor='phone'>Phone:</label>
+        //                     <input type='tel' id='phone' name='phone' required />
+        //                 </div>
+        //                 <div className={styles.inputContainer}>
+        //                     <label htmlFor='address'>Address:</label>
+        //                     <input type='text' id='address' name='address' required />
+        //                 </div>
+        //                 <div className={styles.inputContainer}>
+        //                     <label htmlFor='country'>Country:</label>
+        //                     <input type='text' id='country' name='country' required />
+        //                 </div>
+        //                 <div className={styles.inputContainer}>
+        //                     <label htmlFor='city'>City:</label>
+        //                     <input type='text' id='city' name='city' required />
+        //                 </div>
+        //                 <div className={styles.inputContainer}>
+        //                     <label htmlFor='zip'>Zip:</label>
+        //                     <input type='text' id='zip' name='zip' required />
+        //                 </div>
+        //             </div>
+        //             <div className={styles.paymentInfo}>
+        //                 <select name="" id="">
+        //                     <option value="1">Credit Card</option>
+        //                     <option value="2">Debit Card</option>
+        //                     <option value="3">Paypal</option>
+        //                 </select>
+        //                 <p>TOTAL: {total} €</p>
+        //             </div>
+        //         </div>
 
 
-                <button>Pay</button>
-            </form>
-        </div>
+        //         <button>Pay</button>
+        //     </form>
+        // </div>
     );
 
 }
