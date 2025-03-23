064abe06dcf8c22893920b9d9ab5e5e51c044bf2
Piterxus <piterxus@gmail.com>
2025-03-23 12:52:48 +0100

    feat: Add remove item in cards in principal page

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,7 @@ import Icon from "../components/Icon.tsx";
 import AddToCartForm from "./AddToCartForm";
 import styles from "../styles/Card.module.css";
 import { useStore } from '@nanostores/react';
-import { cartItems, addItemToCart } from '../cartStore';
+import { cartItems, addItemToCart, removeItemFromCart } from '../cartStore';
 
 type CardProps = {
     documentId: string;
@@ -21,6 +21,10 @@ const Card = ({ documentId, price, imageSrc, name }: CardProps) => {
     const handleAddToCart = () => {
         addItemToCart({ id: documentId, price, name, imageSrc, quantity: 1 });
     };
+    const handleRemoveFromCart = () => {
+        removeItemFromCart(documentId);
+    }
+
 
     useEffect(() => {
         setImgSrc(imageSrc);
@@ -33,17 +37,30 @@ const Card = ({ documentId, price, imageSrc, name }: CardProps) => {
             ) : (
                 <p>Loading...</p>
             )}
-            <div className={styles.add_cart}>
-                <p>{price} €</p>
-                {itemExists ? <p>Item in cart</p> : ""}
-                <div onClick={handleAddToCart} style={{ cursor: "pointer" }}>
-                    <Icon
-                        Imgsrc="/imgs/skeleton_shopping_add_cart.png"
-                        alt="Cart"
-                        tooltipText="Add to cart"
-                    />
+            {itemExists ?
+                <div className={styles.add_cart}>
+                    <p>{price} €</p>
+                     <p>Item in cart</p> 
+                    <div onClick={() => removeItemFromCart(documentId)} style={{ cursor: "pointer" }}>
+                        <Icon
+                            Imgsrc="/imgs/skeleton_shopping_remove_cart.png"
+                            alt="Cart"
+                            tooltipText="Remove to cart"
+                        />
+                    </div>
                 </div>
-            </div>
+                :
+                <div className={styles.add_cart}>
+                    <p>{price} €</p>
+             
+                    <div onClick={handleAddToCart} style={{ cursor: "pointer" }}>
+                        <Icon
+                            Imgsrc="/imgs/skeleton_shopping_add_cart.png"
+                            alt="Cart"
+                            tooltipText="Add to cart"
+                        />
+                    </div>
+                </div>}
         </div>
     );
 };
