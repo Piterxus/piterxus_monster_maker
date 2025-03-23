fd9cdce211e2217893649840dc2ff176abab7f94
Piterxus <piterxus@gmail.com>
2025-03-23 14:07:26 +0100

    feat: Add form iin PaymentElement

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -1,10 +1,11 @@
-import { PaymentElement } from "@stripe/react-stripe-js";
+import { PaymentElement, AddressElement } from "@stripe/react-stripe-js";
 
 const PaymentForm = () => {
     return (
         <div>
             <h2>Payment</h2>
             <form>
+                <AddressElement options={{ mode: 'shipping' }} />
                 <PaymentElement />
                 <button type="submit">Pay</button>
             </form>
